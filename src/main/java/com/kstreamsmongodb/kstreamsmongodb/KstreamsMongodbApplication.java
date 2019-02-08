472a5b5792d1eb1527cfc3531d4833c74d3a500c
furkanbrgl <furkan.birgul@inomera.com>
2019-02-08 13:57:16 +0300

    Initial commit

---
+++ src/main/java/com/kstreamsmongodb/kstreamsmongodb/KstreamsMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.kstreamsmongodb.kstreamsmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstreamsMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KstreamsMongodbApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/kstreamsmongodb/kstreamsmongodb/KstreamsMongodbApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kstreamsmongodb.kstreamsmongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KstreamsMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
